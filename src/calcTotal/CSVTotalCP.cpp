958c052d3d43ffbec57c0836c3b0256b7c554d4a
spdevapp <spdevapp@joara.com>
2018-07-05 10:17:17 +0900

    Total calculate 할 때, CSV 출력을 위해, 헤더 입력방식 추가 수정 (헤더 -> 행에 직접 입력)

--- src/calcTotal/CSVTotalCP.cpp
+++ src/calcTotal/CSVTotalCP.cpp
@@ -48,7 +48,40 @@ void CSVTotalCP::InitCPData()
 	m_CSVModel = new QStandardItemModel(ROW_AMOUNT_MAX, CalcCPDlg::CP_MAX + 4);
 
 	// set header
-	m_CSVModel->setHeaderData(0, Qt::Horizontal, QString::fromLocal8Bit("ÃÑ°è"));
+	//m_CSVModel->setHeaderData(0, Qt::Horizontal, QString::fromLocal8Bit("ÃÑ°è"));
+	//m_CSVModel->setHeaderData(CalcCPDlg::CP_KYOBO + 1, Qt::Horizontal, QString::fromLocal8Bit(CP_NAME.at(CalcCPDlg::CP_KYOBO).toStdString().c_str()));
+	//m_CSVModel->setHeaderData(CalcCPDlg::CP_NAVER + 1, Qt::Horizontal, QString::fromLocal8Bit(CP_NAME.at(CalcCPDlg::CP_NAVER).toStdString().c_str()));
+	//m_CSVModel->setHeaderData(CalcCPDlg::CP_RIDI + 1, Qt::Horizontal, QString::fromLocal8Bit(CP_NAME.at(CalcCPDlg::CP_RIDI).toStdString().c_str()));
+	//m_CSVModel->setHeaderData(CalcCPDlg::CP_MUNPIA + 1, Qt::Horizontal, QString::fromLocal8Bit(CP_NAME.at(CalcCPDlg::CP_MUNPIA).toStdString().c_str()));
+	//m_CSVModel->setHeaderData(CalcCPDlg::CP_MRBLUE + 1, Qt::Horizontal, QString::fromLocal8Bit(CP_NAME.at(CalcCPDlg::CP_MRBLUE).toStdString().c_str()));
+	//m_CSVModel->setHeaderData(CalcCPDlg::CP_BAROBOOK + 1, Qt::Horizontal, QString::fromLocal8Bit(CP_NAME.at(CalcCPDlg::CP_BAROBOOK).toStdString().c_str()));
+	//m_CSVModel->setHeaderData(CalcCPDlg::CP_BOOKCUBE + 1, Qt::Horizontal, QString::fromLocal8Bit(CP_NAME.at(CalcCPDlg::CP_BOOKCUBE).toStdString().c_str()));
+	//m_CSVModel->setHeaderData(CalcCPDlg::CP_EPYRUS + 1, Qt::Horizontal, QString::fromLocal8Bit(CP_NAME.at(CalcCPDlg::CP_EPYRUS).toStdString().c_str()));
+	//m_CSVModel->setHeaderData(CalcCPDlg::CP_OEBOOK + 1, Qt::Horizontal, QString::fromLocal8Bit(CP_NAME.at(CalcCPDlg::CP_OEBOOK).toStdString().c_str()));
+	//m_CSVModel->setHeaderData(CalcCPDlg::CP_ONESTORE + 1, Qt::Horizontal, QString::fromLocal8Bit(CP_NAME.at(CalcCPDlg::CP_ONESTORE).toStdString().c_str()));
+	//m_CSVModel->setHeaderData(CalcCPDlg::CP_KAKAO + 1, Qt::Horizontal, QString::fromLocal8Bit(CP_NAME.at(CalcCPDlg::CP_KAKAO).toStdString().c_str()));
+	//m_CSVModel->setHeaderData(CalcCPDlg::CP_COMICO + 1, Qt::Horizontal, QString::fromLocal8Bit(CP_NAME.at(CalcCPDlg::CP_COMICO).toStdString().c_str()));
+	//m_CSVModel->setHeaderData(CalcCPDlg::CP_TOCSODA + 1, Qt::Horizontal, QString::fromLocal8Bit(CP_NAME.at(CalcCPDlg::CP_TOCSODA).toStdString().c_str()));
+	//m_CSVModel->setHeaderData(CalcCPDlg::CP_JUSTOON + 1, Qt::Horizontal, QString::fromLocal8Bit(CP_NAME.at(CalcCPDlg::CP_JUSTOON).toStdString().c_str()));
+	//m_CSVModel->setHeaderData(CalcCPDlg::CP_GOOGLE + 1, Qt::Horizontal, QString::fromLocal8Bit(CP_NAME.at(CalcCPDlg::CP_GOOGLE).toStdString().c_str()));
+	//m_CSVModel->setHeaderData(CalcCPDlg::CP_KEPUB + 1, Qt::Horizontal, QString::fromLocal8Bit(CP_NAME.at(CalcCPDlg::CP_KEPUB).toStdString().c_str()));
+	m_CSVModel->setData(m_CSVModel->index(ROW_AMOUNT_HEADER, 0), QString::fromLocal8Bit("ÃÑ°è"));
+	m_CSVModel->setData(m_CSVModel->index(ROW_AMOUNT_HEADER, CalcCPDlg::CP_KYOBO + 1), QString::fromLocal8Bit(CP_NAME.at(CalcCPDlg::CP_KYOBO).toStdString().c_str()));
+	m_CSVModel->setData(m_CSVModel->index(ROW_AMOUNT_HEADER, CalcCPDlg::CP_NAVER + 1), QString::fromLocal8Bit(CP_NAME.at(CalcCPDlg::CP_NAVER).toStdString().c_str()));
+	m_CSVModel->setData(m_CSVModel->index(ROW_AMOUNT_HEADER, CalcCPDlg::CP_RIDI + 1), QString::fromLocal8Bit(CP_NAME.at(CalcCPDlg::CP_RIDI).toStdString().c_str()));
+	m_CSVModel->setData(m_CSVModel->index(ROW_AMOUNT_HEADER, CalcCPDlg::CP_MUNPIA + 1), QString::fromLocal8Bit(CP_NAME.at(CalcCPDlg::CP_MUNPIA).toStdString().c_str()));
+	m_CSVModel->setData(m_CSVModel->index(ROW_AMOUNT_HEADER, CalcCPDlg::CP_MRBLUE + 1), QString::fromLocal8Bit(CP_NAME.at(CalcCPDlg::CP_MRBLUE).toStdString().c_str()));
+	m_CSVModel->setData(m_CSVModel->index(ROW_AMOUNT_HEADER, CalcCPDlg::CP_BAROBOOK + 1), QString::fromLocal8Bit(CP_NAME.at(CalcCPDlg::CP_BAROBOOK).toStdString().c_str()));
+	m_CSVModel->setData(m_CSVModel->index(ROW_AMOUNT_HEADER, CalcCPDlg::CP_BOOKCUBE + 1), QString::fromLocal8Bit(CP_NAME.at(CalcCPDlg::CP_BOOKCUBE).toStdString().c_str()));
+	m_CSVModel->setData(m_CSVModel->index(ROW_AMOUNT_HEADER, CalcCPDlg::CP_EPYRUS + 1), QString::fromLocal8Bit(CP_NAME.at(CalcCPDlg::CP_EPYRUS).toStdString().c_str()));
+	m_CSVModel->setData(m_CSVModel->index(ROW_AMOUNT_HEADER, CalcCPDlg::CP_OEBOOK + 1), QString::fromLocal8Bit(CP_NAME.at(CalcCPDlg::CP_OEBOOK).toStdString().c_str()));
+	m_CSVModel->setData(m_CSVModel->index(ROW_AMOUNT_HEADER, CalcCPDlg::CP_ONESTORE + 1), QString::fromLocal8Bit(CP_NAME.at(CalcCPDlg::CP_ONESTORE).toStdString().c_str()));
+	m_CSVModel->setData(m_CSVModel->index(ROW_AMOUNT_HEADER, CalcCPDlg::CP_KAKAO + 1), QString::fromLocal8Bit(CP_NAME.at(CalcCPDlg::CP_KAKAO).toStdString().c_str()));
+	m_CSVModel->setData(m_CSVModel->index(ROW_AMOUNT_HEADER, CalcCPDlg::CP_COMICO + 1), QString::fromLocal8Bit(CP_NAME.at(CalcCPDlg::CP_COMICO).toStdString().c_str()));
+	m_CSVModel->setData(m_CSVModel->index(ROW_AMOUNT_HEADER, CalcCPDlg::CP_TOCSODA + 1), QString::fromLocal8Bit(CP_NAME.at(CalcCPDlg::CP_TOCSODA).toStdString().c_str()));
+	m_CSVModel->setData(m_CSVModel->index(ROW_AMOUNT_HEADER, CalcCPDlg::CP_JUSTOON + 1), QString::fromLocal8Bit(CP_NAME.at(CalcCPDlg::CP_JUSTOON).toStdString().c_str()));
+	m_CSVModel->setData(m_CSVModel->index(ROW_AMOUNT_HEADER, CalcCPDlg::CP_GOOGLE + 1), QString::fromLocal8Bit(CP_NAME.at(CalcCPDlg::CP_GOOGLE).toStdString().c_str()));
+	m_CSVModel->setData(m_CSVModel->index(ROW_AMOUNT_HEADER, CalcCPDlg::CP_KEPUB + 1), QString::fromLocal8Bit(CP_NAME.at(CalcCPDlg::CP_KEPUB).toStdString().c_str()));
 
 	// set line number
 	m_CSVModel->setVerticalHeaderItem(ROW_AMOUNT_TOTAL, new QStandardItem("Total Amount"));		// total
@@ -110,135 +143,122 @@ void CSVTotalCP::SetItem(CalcCPDlg* cpData)
 	InitCPData();
 
 	if (cpData->GetKyobo()) {
-		m_CSVModel->setHeaderData(CalcCPDlg::CP_KYOBO + 1, Qt::Horizontal, QString::fromLocal8Bit(CP_NAME.at(CalcCPDlg::CP_KYOBO).toStdString().c_str()));
 		SetCPTable(CalcCPDlg::CP_KYOBO + 1
 			, QString("%L1").arg(cpData->GetKyobo()->GetTotalAmount().at(CSVKyobo::BUSINESS_ALL), 0, 'f', 0)
 			, QString("%L1").arg(cpData->GetKyobo()->GetCalcAmount().at(CSVKyobo::BUSINESS_ALL), 0, 'f', 0)
 			, QString("%L1").arg(cpData->GetKyobo()->GetAuthorAmount().at(CSVKyobo::BUSINESS_ALL), 0, 'f', 0));
 	}
 	if (cpData->GetNaver()) {
-		m_CSVModel->setHeaderData(CalcCPDlg::CP_NAVER + 1, Qt::Horizontal, QString::fromLocal8Bit(CP_NAME.at(CalcCPDlg::CP_NAVER).toStdString().c_str()));
 		SetCPTable(CalcCPDlg::CP_NAVER + 1
 			, QString("%L1").arg(cpData->GetNaver()->GetTotalAmount(), 0, 'f', 0)
 			, QString("%L1").arg(cpData->GetNaver()->GetCalcAmount(), 0, 'f', 0)
 			, QString("%L1").arg(cpData->GetNaver()->GetAuthorAmount(), 0, 'f', 0));
 	}
 	if (cpData->GetRidi()) {
-		m_CSVModel->setHeaderData(CalcCPDlg::CP_RIDI + 1, Qt::Horizontal, QString::fromLocal8Bit(CP_NAME.at(CalcCPDlg::CP_RIDI).toStdString().c_str()));
 		SetCPTable(CalcCPDlg::CP_RIDI + 1
 			, QString("%L1").arg(cpData->GetRidi()->GetTotalAmount(), 0, 'f', 0)
 			, QString("%L1").arg(cpData->GetRidi()->GetCalcAmount(), 0, 'f', 0)
 			, QString("%L1").arg(cpData->GetRidi()->GetAuthorAmount(), 0, 'f', 0));
 	}
 	if (cpData->GetMunpia()) {
-		m_CSVModel->setHeaderData(CalcCPDlg::CP_MUNPIA + 1, Qt::Horizontal, QString::fromLocal8Bit(CP_NAME.at(CalcCPDlg::CP_MUNPIA).toStdString().c_str()));
 		SetCPTable(CalcCPDlg::CP_MUNPIA + 1
 			, QString("%L1").arg(cpData->GetMunpia()->GetTotalAmount(), 0, 'f', 0)
 			, QString("%L1").arg(cpData->GetMunpia()->GetCalcAmount(), 0, 'f', 0)
 			, QString("%L1").arg(cpData->GetMunpia()->GetAuthorAmount(), 0, 'f', 0));
 	}
 	if (cpData->GetMrblue()) {
-		m_CSVModel->setHeaderData(CalcCPDlg::CP_MRBLUE + 1, Qt::Horizontal, QString::fromLocal8Bit(CP_NAME.at(CalcCPDlg::CP_MRBLUE).toStdString().c_str()));
 		SetCPTable(CalcCPDlg::CP_MRBLUE + 1
 			, QString("%L1").arg(cpData->GetMrblue()->GetTotalAmount(), 0, 'f', 0)
 			, QString("%L1").arg(cpData->GetMrblue()->GetCalcAmount(), 0, 'f', 0)
 			, QString("%L1").arg(cpData->GetMrblue()->GetAuthorAmount(), 0, 'f', 0));
 	}
 	if (cpData->GetBarobook()) {
-		m_CSVModel->setHeaderData(CalcCPDlg::CP_BAROBOOK + 1, Qt::Horizontal, QString::fromLocal8Bit(CP_NAME.at(CalcCPDlg::CP_BAROBOOK).toStdString().c_str()));
 		SetCPTable(CalcCPDlg::CP_BAROBOOK + 1
 			, QString("%L1").arg(cpData->GetBarobook()->GetTotalAmount(), 0, 'f', 0)
 			, QString("%L1").arg(cpData->GetBarobook()->GetCalcAmount(), 0, 'f', 0)
 			, QString("%L1").arg(cpData->GetBarobook()->GetAuthorAmount(), 0, 'f', 0));
 	}
 	if (cpData->GetBookcube()) {
-		m_CSVModel->setHeaderData(CalcCPDlg::CP_BOOKCUBE + 1, Qt::Horizontal, QString::fromLocal8Bit(CP_NAME.at(CalcCPDlg::CP_BOOKCUBE).toStdString().c_str()));
 		SetCPTable(CalcCPDlg::CP_BOOKCUBE + 1
 			, QString("%L1").arg(cpData->GetBookcube()->GetTotalAmount(), 0, 'f', 0)
 			, QString("%L1").arg(cpData->GetBookcube()->GetCalcAmount(), 0, 'f', 0)
 			, QString("%L1").arg(cpData->GetBookcube()->GetAuthorAmount(), 0, 'f', 0));
 	}
 	if (cpData->GetEpyrus()) {
-		m_CSVModel->setHeaderData(CalcCPDlg::CP_EPYRUS + 1, Qt::Horizontal, QString::fromLocal8Bit(CP_NAME.at(CalcCPDlg::CP_EPYRUS).toStdString().c_str()));
 		SetCPTable(CalcCPDlg::CP_EPYRUS + 1
 			, QString("%L1").arg(cpData->GetEpyrus()->GetTotalAmount(), 0, 'f', 0)
 			, QString("%L1").arg(cpData->GetEpyrus()->GetCalcAmount(), 0, 'f', 0)
 			, QString("%L1").arg(cpData->GetEpyrus()->GetAuthorAmount(), 0, 'f', 0));
 	}
 	if (cpData->GetOebook()) {
-		m_CSVModel->setHeaderData(CalcCPDlg::CP_OEBOOK + 1, Qt::Horizontal, QString::fromLocal8Bit(CP_NAME.at(CalcCPDlg::CP_OEBOOK).toStdString().c_str()));
 		SetCPTable(CalcCPDlg::CP_OEBOOK + 1
 			, QString("%L1").arg(cpData->GetOebook()->GetTotalAmount().at(CSVOebook::COMPANY_OEBOOK_ALL), 0, 'f', 0)
 			, QString("%L1").arg(cpData->GetOebook()->GetCalcAmount().at(CSVOebook::COMPANY_OEBOOK_ALL), 0, 'f', 0)
 			, QString("%L1").arg(cpData->GetOebook()->GetAuthorAmount().at(CSVOebook::COMPANY_OEBOOK_ALL), 0, 'f', 0));
 	}
 	if (cpData->GetOnestore()) {
-		m_CSVModel->setHeaderData(CalcCPDlg::CP_ONESTORE + 1, Qt::Horizontal, QString::fromLocal8Bit(CP_NAME.at(CalcCPDlg::CP_ONESTORE).toStdString().c_str()));
 		SetCPTable(CalcCPDlg::CP_ONESTORE + 1
 			, QString("%L1").arg(cpData->GetOnestore()->GetTotalAmount(), 0, 'f', 0)
 			, QString("%L1").arg(cpData->GetOnestore()->GetCalcAmount(), 0, 'f', 0)
 			, QString("%L1").arg(cpData->GetOnestore()->GetAuthorAmount(), 0, 'f', 0));
 	}
 	if (cpData->GetKakao()) {
-		m_CSVModel->setHeaderData(CalcCPDlg::CP_KAKAO + 1, Qt::Horizontal, QString::fromLocal8Bit(CP_NAME.at(CalcCPDlg::CP_KAKAO).toStdString().c_str()));
 		SetCPTable(CalcCPDlg::CP_KAKAO + 1
 			, QString("%L1").arg(cpData->GetKakao()->GetTotalAmount(), 0, 'f', 0)
 			, QString("%L1").arg(cpData->GetKakao()->GetCalcAmount(), 0, 'f', 0)
 			, QString("%L1").arg(cpData->GetKakao()->GetAuthorAmount(), 0, 'f', 0));
 	}
 	if (cpData->GetComico()) {
-		m_CSVModel->setHeaderData(CalcCPDlg::CP_COMICO + 1, Qt::Horizontal, QString::fromLocal8Bit(CP_NAME.at(CalcCPDlg::CP_COMICO).toStdString().c_str()));
 		SetCPTable(CalcCPDlg::CP_COMICO + 1
 			, QString("%L1").arg(cpData->GetComico()->GetTotalAmount(), 0, 'f', 0)
 			, QString("%L1").arg(cpData->GetComico()->GetCalcAmount(), 0, 'f', 0)
 			, QString("%L1").arg(cpData->GetComico()->GetAuthorAmount(), 0, 'f', 0));
 	}
 	if (cpData->GetTocsoda()) {
-		m_CSVModel->setHeaderData(CalcCPDlg::CP_TOCSODA + 1, Qt::Horizontal, QString::fromLocal8Bit(CP_NAME.at(CalcCPDlg::CP_TOCSODA).toStdString().c_str()));
 		SetCPTable(CalcCPDlg::CP_TOCSODA + 1
 			, QString("%L1").arg(cpData->GetTocsoda()->GetTotalAmount(), 0, 'f', 0)
 			, QString("%L1").arg(cpData->GetTocsoda()->GetCalcAmount(), 0, 'f', 0)
 			, QString("%L1").arg(cpData->GetTocsoda()->GetAuthorAmount(), 0, 'f', 0));
 	}
-	if (cpData->GetKepub()) {
-		m_CSVModel->setHeaderData(CalcCPDlg::CP_KEPUB + 1, Qt::Horizontal, QString::fromLocal8Bit(CP_NAME.at(CalcCPDlg::CP_KEPUB).toStdString().c_str()));
-		SetCPTable(CalcCPDlg::CP_KEPUB + 1
-			, QString("%L1").arg(cpData->GetKepub()->GetTotalAmount().at(CSVKepub::STORE_ALL), 0, 'f', 0)
-			, QString("%L1").arg(cpData->GetKepub()->GetCalcAmount().at(CSVKepub::STORE_ALL), 0, 'f', 0)
-			, QString("%L1").arg(cpData->GetKepub()->GetAuthorAmount().at(CSVKepub::STORE_ALL), 0, 'f', 0));
-
-		// YES24
-		m_CSVModel->setHeaderData(CalcCPDlg::CP_KEPUB + 2, Qt::Horizontal, QString::fromLocal8Bit("YES24"));
-		SetCPTable(CalcCPDlg::CP_KEPUB + 2
-			, QString("%L1").arg(cpData->GetKepub()->GetTotalAmount().at(CSVKepub::STORE_YES24), 0, 'f', 0)
-			, QString("%L1").arg(cpData->GetKepub()->GetCalcAmount().at(CSVKepub::STORE_YES24), 0, 'f', 0)
-			, QString("%L1").arg(cpData->GetKepub()->GetAuthorAmount().at(CSVKepub::STORE_YES24), 0, 'f', 0));
-		// ALADDIN
-		m_CSVModel->setHeaderData(CalcCPDlg::CP_KEPUB + 3, Qt::Horizontal, QString::fromLocal8Bit("ALADDIN"));
-		SetCPTable(CalcCPDlg::CP_KEPUB + 3
-			, QString("%L1").arg(cpData->GetKepub()->GetTotalAmount().at(CSVKepub::STORE_ALADDIN), 0, 'f', 0)
-			, QString("%L1").arg(cpData->GetKepub()->GetCalcAmount().at(CSVKepub::STORE_ALADDIN), 0, 'f', 0)
-			, QString("%L1").arg(cpData->GetKepub()->GetAuthorAmount().at(CSVKepub::STORE_ALADDIN), 0, 'f', 0));
-		// BANDI
-		m_CSVModel->setHeaderData(CalcCPDlg::CP_KEPUB + 4, Qt::Horizontal, QString::fromLocal8Bit("BANDI"));
-		SetCPTable(CalcCPDlg::CP_KEPUB + 4
-			, QString("%L1").arg(cpData->GetKepub()->GetTotalAmount().at(CSVKepub::STORE_BANDI), 0, 'f', 0)
-			, QString("%L1").arg(cpData->GetKepub()->GetCalcAmount().at(CSVKepub::STORE_BANDI), 0, 'f', 0)
-			, QString("%L1").arg(cpData->GetKepub()->GetAuthorAmount().at(CSVKepub::STORE_BANDI), 0, 'f', 0));
-	}
 	if (cpData->GetJustoon()) {
-		m_CSVModel->setHeaderData(CalcCPDlg::CP_JUSTOON + 1, Qt::Horizontal, QString::fromLocal8Bit(CP_NAME.at(CalcCPDlg::CP_JUSTOON).toStdString().c_str()));
 		SetCPTable(CalcCPDlg::CP_JUSTOON + 1
 			, QString("%L1").arg(cpData->GetJustoon()->GetTotalAmount(), 0, 'f', 0)
 			, QString("%L1").arg(cpData->GetJustoon()->GetCalcAmount(), 0, 'f', 0)
 			, QString("%L1").arg(cpData->GetJustoon()->GetAuthorAmount(), 0, 'f', 0));
 	}
 	if (cpData->GetGoogle()) {
-		m_CSVModel->setHeaderData(CalcCPDlg::CP_GOOGLE + 1, Qt::Horizontal, QString::fromLocal8Bit(CP_NAME.at(CalcCPDlg::CP_GOOGLE).toStdString().c_str()));
 		SetCPTable(CalcCPDlg::CP_GOOGLE + 1
 			, QString("%L1").arg(cpData->GetGoogle()->GetTotalAmount(), 0, 'f', 0)
 			, QString("%L1").arg(cpData->GetGoogle()->GetCalcAmount(), 0, 'f', 0)
 			, QString("%L1").arg(cpData->GetGoogle()->GetAuthorAmount(), 0, 'f', 0));
+	}
+	if (cpData->GetKepub()) {
+		SetCPTable(CalcCPDlg::CP_KEPUB + 1
+			, QString("%L1").arg(cpData->GetKepub()->GetTotalAmount().at(CSVKepub::STORE_ALL), 0, 'f', 0)
+			, QString("%L1").arg(cpData->GetKepub()->GetCalcAmount().at(CSVKepub::STORE_ALL), 0, 'f', 0)
+			, QString("%L1").arg(cpData->GetKepub()->GetAuthorAmount().at(CSVKepub::STORE_ALL), 0, 'f', 0));
+
+		// YES24
+		//m_CSVModel->setHeaderData(CalcCPDlg::CP_KEPUB + 2, Qt::Horizontal, QString::fromLocal8Bit("YES24"));
+		m_CSVModel->setData(m_CSVModel->index(ROW_AMOUNT_HEADER, CalcCPDlg::CP_KEPUB + 2), QString::fromLocal8Bit("YES24"));
+		SetCPTable(CalcCPDlg::CP_KEPUB + 2
+			, QString("%L1").arg(cpData->GetKepub()->GetTotalAmount().at(CSVKepub::STORE_YES24), 0, 'f', 0)
+			, QString("%L1").arg(cpData->GetKepub()->GetCalcAmount().at(CSVKepub::STORE_YES24), 0, 'f', 0)
+			, QString("%L1").arg(cpData->GetKepub()->GetAuthorAmount().at(CSVKepub::STORE_YES24), 0, 'f', 0));
+		// ALADDIN
+		//m_CSVModel->setHeaderData(CalcCPDlg::CP_KEPUB + 3, Qt::Horizontal, QString::fromLocal8Bit("ALADDIN"));
+		m_CSVModel->setData(m_CSVModel->index(ROW_AMOUNT_HEADER, CalcCPDlg::CP_KEPUB + 3), QString::fromLocal8Bit("ALADDIN"));
+		SetCPTable(CalcCPDlg::CP_KEPUB + 3
+			, QString("%L1").arg(cpData->GetKepub()->GetTotalAmount().at(CSVKepub::STORE_ALADDIN), 0, 'f', 0)
+			, QString("%L1").arg(cpData->GetKepub()->GetCalcAmount().at(CSVKepub::STORE_ALADDIN), 0, 'f', 0)
+			, QString("%L1").arg(cpData->GetKepub()->GetAuthorAmount().at(CSVKepub::STORE_ALADDIN), 0, 'f', 0));
+		// BANDI
+		//m_CSVModel->setHeaderData(CalcCPDlg::CP_KEPUB + 4, Qt::Horizontal, QString::fromLocal8Bit("BANDI"));
+		m_CSVModel->setData(m_CSVModel->index(ROW_AMOUNT_HEADER, CalcCPDlg::CP_KEPUB + 4), QString::fromLocal8Bit("BANDI"));
+		SetCPTable(CalcCPDlg::CP_KEPUB + 4
+			, QString("%L1").arg(cpData->GetKepub()->GetTotalAmount().at(CSVKepub::STORE_BANDI), 0, 'f', 0)
+			, QString("%L1").arg(cpData->GetKepub()->GetCalcAmount().at(CSVKepub::STORE_BANDI), 0, 'f', 0)
+			, QString("%L1").arg(cpData->GetKepub()->GetAuthorAmount().at(CSVKepub::STORE_BANDI), 0, 'f', 0));
 	}
 
 	// total amount
